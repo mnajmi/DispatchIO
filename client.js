88c9049bae7727f58d1b099af9bb7df125acae21
Hafiz Muhammad Najmul Hasnain <mnajmulhasnain@gmail.com>
2020-12-17 21:13:41 +0500

    Add files via upload

--- client.js
+++ client.js
@@ -5,3 +5,62 @@ const
 ioClient.on("seq-num", (msg) => console.info(msg));
 
 console.log('client');
+
+const socket = io()
+
+//let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+//do {
+  //  name = prompt('Please enter your name: ')
+//} while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: '👤 '+name,
+        message: '💬'+message.trim(),
+      ack:"received 📨 "
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+</br>
+</br>
+        <span>${msg.ack}</span>
+<span>----xxxx----</span>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+  console.log(msg)
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-var app = require('express')();
+const express = require("express");
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const port = 8000;
+const SocketHub = require("./public/javascript/sockethub");
+const Logs = require("./public/javascript/Logs");
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(express.static("./public"));
+
+//const sql = require("mssql/msnodesqlv8");
+//var dateFormat = require("dateformat");
+//const util = require('util');
+//const path = require("path");
+//const db=require("./public/javascript/db");
+
 //var nsp = io.of('/DispatchServer');
 /*const sql = require('mssql')
 var config = {
@@ -15,40 +27,42 @@ var config = {
     trustedConnection: true
     }
 };*/
-
+//app.use(express.static(__dirname + '/../../build'))
 
 app.get('/', function(req, res){
     res.sendFile('index.html', { root: __dirname });
 });
 
-
-io.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
+  Logs.CreateLog("On Connection event Called ", socket.id, "OnConnect");
   console.info(`Client connected [id=${socket.id}]`);
+
   //Recieving an event from the client
+  socket.on('LatLong', function(data, callback) { 
+    console.info(data); 
+    socket.emit('Ack', 'Latitude Longitude recieved! ' + data); // return an response event
+    callback('LatLong acknowledgement response....'); 
+  }); // return an event acknowledgment's callback
+
   socket.on('clientEvent', function(data) { console.info(data); });
-  
-  //sending a message to all connected clients
-  socket.emit('broadcast',{ description: ' clients connected!'});
-  
-  //send the new user a welcome message
-  socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
 
-  //send the message
-  socket.emit('hi', 'Hello everyone!');
+  socket.on('clientEventAcknowledge', function(data, callback) { 
+    console.info(data); 
+    callback('clientEvent acknowledgement response....'); 
+  });
 
-  setTimeout(function() {
-    //Send a message after a timeout of 4seconds
-    socket.send('Sent a message to connection, after 5 seconds!');
-    //Sending an object when emmiting an event
+  setInterval(() => {
+    socket.emit('ReceiveMessage', 'connection established message ! ');
     socket.emit('serverEvent', {name: 'test', description: 'A custom event named testerEvent!'});
-  }, 5000);
+   }, 5000);
 
-  socket.on('disconnect', function() {
-    console.info(`Client gone [id=${socket.id}]`);
-    io.sockets.emit('broadcast',{ description: ' clients disconnected!'});
+  socket.on('disconnect', function(reason) {
+    Logs.CreateLog("disconnect press on button : ", socket.id, "disconnect");
+    SocketHub.Disconnect(socket);
+    console.info(`Client gone [id=${socket.id}] reason is ${reason}`);
   });
 });
 
-http.listen(port, () => {
-  console.info('listening on *:' + port);
+http.listen(process.env.PORT, () => {
+  console.info('listening on *:' + process.env.PORT);
 });
